54c53defcd8d10fe506356de241ae9231d707b69
Bibaswan Prasai <bibaswanprasai12@gmail.com>
2023-09-29 13:16:42 +0545

    ui: implemented routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
+import AboutUsPage from './pages/AboutUsPage';
+import FaqPage from './pages/FaqPage';
 
 function App() {
   return (
-    <>
-    <HomePage />
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<HomePage />}/>
+      <Route path='/about' element={<AboutUsPage />}/>
+      <Route path='/faq' element={<FaqPage />}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
